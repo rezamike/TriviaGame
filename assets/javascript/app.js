b8dcadea26b0d9ec96b23979a444987381df57d3
rezamike <michael.sanaiha@gmail.com>
2018-08-29 10:22:03 -0700

    not really any changes here - started over

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+// variable for question and answers
+    let questions = [
+        {
+            question: "What film is considered to have the longest one-take in history?",
+            choices: ["Russian Ark", "Children of Men", "Birdman", "Lost in London"],
+            correct: 3
+        },
+        {
+            question: "In what film did Leonardo DiCaprio actually cut his hand and seriously injure himself during filming?",
+            choices: ["Titanic", "Django Unchained", "Inception", "What's Eating Gilbert Grape?"],
+            correct: 1 
+        },
+        {
+            question: "How long do the rights for movies last?",
+            choices: ["Forever", "33 years", "95 years", "a decade"],
+            correct: 2
+        },
+        {
+            question: "Which car company used a film to reinstate a classic car that was off the market for eight years?",
+            choices: ["Ford", "Fiat", "Dodge", "Chevrolet"],
+            correct: 3
+        }
+    ]
+
+$(document).ready(function () {
+
+    // create function for removing "button" and starting the game with first question
+    
+
+})
